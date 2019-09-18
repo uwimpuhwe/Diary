9924c7cec814d4b416864c5fd853c8ad43ce388f
uwimpuhwe <uwimpuhweamata@akilahinstitute.org>
2019-09-18 17:34:20 +0200

    (ft-speific-entry-168383224) View a specific entries Finished ft-specific-entry-168383224

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import allEntries from '../controllers/getAll';
+import getOne from '../controllers/getEntrybyId';
 
 
 const router = express.Router();
 
 router.get('/api/v1/entries', allEntries);
+router.get('/api/v1/entries/:id', getOne);
 
 
 export default router;
