179983f096b04048230a760d2fa34cee8abc9c92
uwimpuhwe <uwimpuhweamata@akilahinstitute.org>
2019-09-19 12:19:36 +0200

    .gitignore fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# DiaryRepo
-myDairy repository
--- server/controllers/getAll.js
+++
@@ -1,9 +0,0 @@
-import diaryEntry from '../models/entryModel';
-
- const allEntries= (req, res)=> {
-    res.status(200).json({
-        status : 200,
-        entry : diaryEntry,
-    })
-  };
-  export default allEntries;
